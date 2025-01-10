7755185b05a5a7440d5f44e74208de2fb01257fe
sukori <sukori98@gmail.com>
2025-01-10 09:48:27 +0100

    huge advancement

--- src/txt_convert.c
+++ src/txt_convert.c
@@ -6,13 +6,18 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 14:54:13 by pberset           #+#    #+#             */
-/*   Updated: 2025/01/07 15:00:02 by pberset          ###   Lausanne.ch       */
+/*   Updated: 2025/01/10 09:48:04 by pberset          ###   Lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/txt_convert.h"
 
-int	main()
+int	main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		printf("wrong command format\n");
+		return (1);
+	}
 	return (0);
 }
